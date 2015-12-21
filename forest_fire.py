61222499675f443b95a412457d35e32b7528dee1
Alexandr Kalenuk <akalenuk@gmail.com>
2015-12-21 21:59:11 +0200

    empty message

--- forest_fire.py
+++ forest_fire.py
@@ -3,16 +3,14 @@ import least_squares
 import numpy
 import oneliners
 
-EPS = 3.0
-NORM_DOT = 0.6
+EPS = 5.0
+NORM_DOT = 0.9
 
 def ignite(triangle_i, plane_id, plane, plane_map):
+	global triangle_to_planes
+
 	# burnout check
-	burned = 0
-	for vi in triangles[triangle_i]:
-		if plane_id in plane_map[vi]:
-			burned += 1
-	if burned == 3:
+	if len(triangle_to_planes[triangle_i]) != 0:
 		return
 
 	# normal check
@@ -30,18 +28,19 @@ def ignite(triangle_i, plane_id, plane, plane_map):
 
 	# mark plane_map
 	for vi in triangles[triangle_i]:
-		plane_map[vi] += [plane_id]
+		plane_map[vi] = list(set(plane_map[vi] + [plane_id]))
+	triangle_to_planes[triangle_i] += [plane_id]
 
 	# ignite neighbours
-	tris = triangles[triangle_i]
-	for (v1, v2) in zip(tris, tris[1:] + [tris[0]]):
-		vkey = tuple(sorted((v1, v2)))
-		for new_triangle_i in edges_to_triangles[vkey]:
-			ignite(new_triangle_i, plane_id, plane, plane_map)
+	for new_triangle_i in range(len(triangles)):
+		for i in triangles[new_triangle_i]:
+			if i in triangles[triangle_i]:
+				ignite(new_triangle_i, plane_id, plane, plane_map)
+				break;
 
 
 if __name__ == "__main__":
-	f = open('cube.obj', 'r')
+	f = open('ellipsoid.obj', 'r')
 	input_obj = f.read()
 	f.close()
 
@@ -50,54 +49,42 @@ if __name__ == "__main__":
 	triangles = obj_io.triangles(input_obj)
 	triangles = [[ti-1 for ti in tis] for tis in triangles]
 
-	edges_to_triangles = {}
-	for i in range(len(triangles)):
-		tri = triangles[i]
-		for (v1, v2) in zip(tri, tri[1:] + [tri[0]]):
-			vkey = tuple(sorted((v1, v2)))
-			if (v1, v2) in edges_to_triangles:
-				edges_to_triangles[vkey] += [i]
-			else:
-				edges_to_triangles[vkey] = [i]
-
 	normals = obj_io.normals(input_obj)
 	triangle_normals = obj_io.triangle_normals(input_obj)
 	triangle_normals = [[ti-1 for ti in tis] for tis in triangle_normals]
 
 	# make plane_map - vertex_index to list of planes it belongs to	
 	plane_map = [[] for every in vertexes]
+	triangle_to_planes = [[] for every in triangles]
 	for i in range(len(triangles)):
-		tris = sorted(triangles[i])
-		wood = 0
-		for tri in tris:
-			if len(plane_map[tri]) == 0:
-				wood += 1
-		if wood == 0: 
-			continue
+		tris = triangles[i]
  		pts = numpy.array([vertexes[pi] for pi in tris] + [vertexes[pi] for pi in tris])
 		plane = least_squares.fit_plane(pts)
 		ignite(i, i, plane, plane_map)
-		break # just one plane for now
-
 
 	# reverse plane_map, replace every point in group with group centroid
 	# wouldn't work very well for non-convex plane patches, but who's perfect
 	planes_to_vertexes = {}
 	for (vertex_index, set_of_planes) in zip(range(len(plane_map)), plane_map):
-		list_of_planes = list(set_of_planes)
+		list_of_planes = list(set(set_of_planes))
 		str_of_planes = ' '.join([str(p) for p in list_of_planes])
 		if str_of_planes in planes_to_vertexes:
 			planes_to_vertexes[str_of_planes] += [vertex_index]
 		else:
 			planes_to_vertexes[str_of_planes] = [vertex_index]
 
-	for (_, vertex_indexes)	in planes_to_vertexes.iteritems():
-		print len(vertex_indexes),
+	for (a, b) in planes_to_vertexes.iteritems():
+		print a, b
+
+	for (planes, vertex_indexes) in planes_to_vertexes.iteritems():
+		if len(planes) == 0:	# debug case, generally there shouldn't be any unplaned vertexes
+			continue
+		centroid = [0., 0., 0.]	
 		div = 1. / len(vertex_indexes)
-		centroid = [0., 0., 0.]
 		for vi in vertex_indexes:
 			centroid = [c+v for (c, v) in zip(centroid, vertexes[vi])]
 		centroid = [c * div for c in centroid]
+	
 		for vi in vertex_indexes:
 			vertexes[vi] = [c for c in centroid]
 			
