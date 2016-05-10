92605bb0afc3575126684b376b75e2f1b410a856
Alexandr Kalenuk <akalenuk@gmail.com>
2016-05-10 21:03:07 +0300

    empty message

--- la_exp.py
+++ la_exp.py
@@ -1,45 +1,44 @@
 from oneliners import *
 from types import *
 from random import randint
+import time
 
 def v__E(a):
-    ''' Levi-Civita symbol '''
-    n=0
-    t=[]
-    for ti in a:
-        t.append(ti)
-    for i in xrange(0,len(a)):
-        for j in xrange(0,len(a)-i-1):
-            if t[j]==t[j+1]:
-                return 0
-            elif t[j]>t[j+1]:
-                n+=1
-                t.insert(j+1,t.pop(j))
-    if n%2==0:return 1
-    return -1
+	''' Levi-Civita symbol '''
+	n=0
+	t=[ti for ti in a]
+	for i in xrange(0,len(a)):
+		for j in xrange(0,len(a)-i-1):
+			if t[j]==t[j+1]:
+				return 0
+			if t[j]>t[j+1]:
+				n+=1
+				t[j], t[j+1] = t[j+1], t[j]
+	if n % 2 == 0:
+		return 1
+	else:
+		return -1
 
 
-def v_cross(A):
-    ''' n-dimensional cross product on (n-1) vectors in list A '''
-    for a in A:
-        assert len(a) == len(A[0]), "Vector size mismatch in 'v_cross'"
-    DIMM=len(A[0])
-    N=len(A)
-    assert N == DIMM-1, "Vector number mismatch in 'v_cross'"
+def cross_of(A):
+	''' n-dimensional cross product on (n-1) vectors in list A '''
+	for a in A:
+		assert len(a) == len(A[0]), "Vector size mismatch in 'cross_of'"
+	DIMM = len(A[0])
+	N = len(A)
+	assert N == DIMM-1, "Vector number mismatch in 'cross_of'"
 
-    v_res=[]
-    for i in xrange(0,DIMM):
-        v_res.append(0.0)
-        for jk in xrange(0,DIMM**N):
-            v_ijk=[i]
-            for j in xrange(0,N):
-                v_ijk.append((jk/(DIMM**(N-j-1)))%DIMM)
-            t_res=v__E(v_ijk)
-            if t_res!=0:
-                for k in xrange(0,N):
-                    t_res*=A[k][v_ijk[k+1]]
-                v_res[i]+=t_res
-    return v_res
+	v_res = [0.] * DIMM
+	for i in xrange(0, DIMM):
+		for jk in xrange(0, DIMM**N):
+			v_ijk = [i] + [(jk/(DIMM ** (N-j-1))) % DIMM for j in range(0, N)]
+			t_res=v__E(v_ijk)
+			if t_res != 0:
+				for k in xrange(0,N):
+					t_res *= A[k][v_ijk[k + 1]]
+				v_res[i] += t_res
+	return v_res
+
 
 def point_on_plane(n, d):
 	return [-d/n[0]] + [0]*(len(n)-1)
@@ -59,7 +58,7 @@ def exp_solution_for(A, B):
 		plane_n = A[i]
 		plane_d = -B[i]
 		other_planes_ns = A[:i] + A[i+1:]
-		projection_vector = v_cross(other_planes_ns)
+		projection_vector = cross_of(other_planes_ns)
 		p = project_by_vector(p, projection_vector, plane_n, plane_d)
 	return p
 
@@ -85,6 +84,7 @@ if __name__ == '__main__':
 	projected_point = project_by_vector(point_to_project, projection_vector, plane_n, plane_d)	
 	print 'Projected point equation fit:', dot_of(plane_n, projected_point) + plane_d
 	
+	timestamp = time.clock()
 	dimm = 5
 	for n in range(0, 100):
 		A = [[] for each in range(dimm)]
@@ -99,4 +99,5 @@ if __name__ == '__main__':
 			X = exp_solution_for(A, B)
 			print Validate(A, B, X)
 		except ZeroDivisionError:
-			print 'No'	
+			print 'No solution'
+	print 'In', time.clock() - timestamp, 'seconds'
