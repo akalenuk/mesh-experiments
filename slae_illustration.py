9dc0087eba5ac76492b08bbba3026eafaa0d4e27
Oleksandr Kaleniuk <akalenuk@gmaiul.com>
2017-06-04 14:13:22 +0300

    SLAE solution illustration

---
+++ slae_illustration.py
@@ -0,0 +1,56 @@
+from oneliners import *
+from html_bitmap import *
+
+
+A = [[-0.35, 1], [-1.25, 1]]
+B = [2, -2]
+X = [1, 1]
+SHOW_ITERATIVE = True
+
+COLORS =[
+	to_hex(200, 40, 90),
+	to_hex(70, 40, 200),
+	to_hex(70, 180, 90)
+]
+
+def x2s(x):
+	return 100*x
+
+def y2s(y):
+	return 400-100*y
+
+def solution_iteration(bitmap, A, B, Xi, i):
+	if distance_between(multiplication_of(A, Xi), B) < 0.0001:
+		return Xi
+	else:
+		Xj = projected_on_plane(Xi, A[i % len(B)], B[i % len(B)])
+		line_on(bitmap, x2s(Xi[0]), y2s(Xi[1]), x2s(Xj[0]), y2s(Xj[1]), to_hex(180, 200, 190), 1)
+		return solution_iteration(bitmap, A, B, Xj, i+1)
+
+
+def add_grid_to(bitmap):
+	line_on(bitmap, 0, 0, 0, 399, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 0, 0, 7, 20, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 0, 99, 7, 99, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 0, 199, 7, 199, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 0, 299, 7, 299, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 0, 399, 499, 399, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 479, 392, 499, 399, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 99, 399, 99, 392, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 199, 399, 199, 392, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 299, 399, 299, 392, to_hex(0, 0, 0), 1)
+	line_on(bitmap, 399, 399, 399, 392, to_hex(0, 0, 0), 1)
+
+def add_lines(bitmap):
+	for i in range(len(B)):
+		line_on(bitmap, x2s(0), y2s(B[i]), x2s(5), y2s(B[i] - A[i][0] * 5), COLORS[i], 2)
+#	line_on(bitmap, x2s(0), y2s(2), x2s(5), y2s(3.25), to_hex(200, 40, 90), 2)
+#	line_on(bitmap, x2s(0), y2s(-2), x2s(5), y2s(4.25), to_hex(70, 40, 200), 2)
+
+if __name__ == "__main__":
+	bitmap = new_bitmap(500, 400)	
+	if SHOW_ITERATIVE:
+		solution_iteration(bitmap, A, B, X, 1)
+	add_lines(bitmap)
+	add_grid_to(bitmap)	
+	print to_html(bitmap)
