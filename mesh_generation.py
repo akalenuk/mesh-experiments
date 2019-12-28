63d633d2892ad0f8e1b9b92d835d949c3b50f82f
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-12-28 16:32:20 +0200

    small bugs

--- mesh_generation.py
+++ mesh_generation.py
@@ -17,8 +17,8 @@ def snap(f, xyz):
 
 def add_tri(f, a, p1, p2, p0, i1, i2, in1, in2, vs, ts):
     old_norm = normalized(cross_of(sub_of(p0, p1), sub_of(p2, p1)))
-    new_point_dir = normialized(cross_of(old_norm, sub_of(p2, p1)))
-    new_point = sum_of(scaled(sum_of(p1, p2), 0.5), scaled(new_point_dir, a)
+    new_point_dir = normalized(cross_of(old_norm, sub_of(p2, p1)))
+    new_point = sum_of(scaled(sum_of(p1, p2), 0.5), scaled(new_point_dir, a))
     new_snapped_point = snap(f, new_point)
     in_vs = False
     new_point_i = len(vs)
@@ -33,8 +33,8 @@ def add_tri(f, a, p1, p2, p0, i1, i2, in1, in2, vs, ts):
     if not in_vs:
         vs += [new_snapped_point]
     ts += [[i1, i2, new_point_i]]
-    add_tri(f, a, p1, new_snapped_point, i1, new_point_i, in1, in_vs, vs, ts)
-    add_tri(f, a, p2, new_snapped_point, i2, new_point_i, in2, in_vs, vs, ts)
+    add_tri(f, a, p1, new_snapped_point, p2, i1, new_point_i, in1, in_vs, vs, ts)
+    add_tri(f, a, p2, new_snapped_point, p1, i2, new_point_i, in2, in_vs, vs, ts)
 
 
 if __name__ == "__main__":
@@ -42,12 +42,12 @@ if __name__ == "__main__":
     ts = []
 
     first_tri_ps = [snap(SDF, [0., 0., 1.]), snap(SDF, [0., 0.1, 1.]), snap(SDF, [0.1, 0., 1.])]
-    vs += [first_tri_ps]
+    vs += first_tri_ps
     ts += [0, 1, 2]
 
-    add_tri(SDF, 0.8, first_tri_ps[0], first_tri_ps[1], 0, 1, True, True, vs, ts)
-    add_tri(SDF, 0.8, first_tri_ps[1], first_tri_ps[2], 1, 2, True, True, vs, ts)
-    add_tri(SDF, 0.8, first_tri_ps[2], first_tri_ps[0], 2, 0, True, True, vs, ts)
+    add_tri(SDF, 0.8, first_tri_ps[0], first_tri_ps[1],  first_tri_ps[2], 0, 1, True, True, vs, ts)
+    add_tri(SDF, 0.8, first_tri_ps[1], first_tri_ps[2],  first_tri_ps[0], 1, 2, True, True, vs, ts)
+    add_tri(SDF, 0.8, first_tri_ps[2], first_tri_ps[0],  first_tri_ps[1], 2, 0, True, True, vs, ts)
 
     print ("triangles: " + str(len(ts)) + "   vertices: " + str(len(vs)))
 
