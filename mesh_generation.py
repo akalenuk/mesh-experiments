591c6ff0ff7ca3200bfc40b853cda376483ac712
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-12-28 18:24:00 +0200

    better start

--- mesh_generation.py
+++ mesh_generation.py
@@ -15,7 +15,9 @@ def grad(f, xyz):
 def snap(f, xyz):
     return sum_of(xyz, scaled(normalized(grad(f, xyz)), -f(xyz)))
 
-def add_tri(f, a, p1, p2, p0, i1, i2, in1, in2, vs, ts):
+def add_tri(f, a, p1, p2, p0, i1, i2, in1, in2, vs, ts, x):
+    if x == 0:
+        return
     old_norm = normalized(cross_of(sub_of(p0, p1), sub_of(p2, p1)))
     new_point_dir = normalized(cross_of(old_norm, sub_of(p2, p1)))
     new_point = sum_of(scaled(sum_of(p1, p2), 0.5), scaled(new_point_dir, a))
@@ -33,22 +35,20 @@ def add_tri(f, a, p1, p2, p0, i1, i2, in1, in2, vs, ts):
     if not in_vs:
         vs += [new_snapped_point]
     ts += [[i1, i2, new_point_i]]
-    add_tri(f, a, p1, new_snapped_point, p2, i1, new_point_i, in1, in_vs, vs, ts)
-    add_tri(f, a, p2, new_snapped_point, p1, i2, new_point_i, in2, in_vs, vs, ts)
+    add_tri(f, a, p1, new_snapped_point, p2, i1, new_point_i, in1, in_vs, vs, ts, x-1)
+    add_tri(f, a, p2, new_snapped_point, p1, i2, new_point_i, in2, in_vs, vs, ts, x-1)
 
 
 if __name__ == "__main__":
     vs = []
     ts = []
+    a = 0.4
 
     first_tri_ps = [snap(SDF, [0., 0., 1.]), snap(SDF, [0., 0.1, 1.]), snap(SDF, [0.1, 0., 1.])]
-    vs += first_tri_ps
-    ts += [0, 1, 2]
+    vs += [snap(SDF, [0., 0., 1.]), snap(SDF, [0., a, 1.])]
 
-    add_tri(SDF, 0.8, first_tri_ps[0], first_tri_ps[1],  first_tri_ps[2], 0, 1, True, True, vs, ts)
-    add_tri(SDF, 0.8, first_tri_ps[1], first_tri_ps[2],  first_tri_ps[0], 1, 2, True, True, vs, ts)
-    add_tri(SDF, 0.8, first_tri_ps[2], first_tri_ps[0],  first_tri_ps[1], 2, 0, True, True, vs, ts)
-
+    add_tri(SDF, a, vs[0], vs[1],  snap(SDF, [a, 0., 1.]), 0, 1, True, True, vs, ts, 4)
+    
     print ("triangles: " + str(len(ts)) + "   vertices: " + str(len(vs)))
 
     f = open(OUTPUT, 'w')
